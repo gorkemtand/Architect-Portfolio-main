211b7107928a51cec480228c97336a770c4285b6
Görkem <tandogangorkem@gmail.com>
2024-04-15 01:05:07 +0300

    -logo images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
     <nav>
       <div className="navbar-content">
         <div className='navbarTitle'>
-          <img src="https://storage.googleapis.com/gorkemtandogan-images/Projects/Architect%20Portfolio/logo.png" alt='logo'></img>
+          <img src="/images/logo.png" alt='logo'></img>
           <h1>BUZ DESIGN</h1>
         </div>
 
@@ -24,13 +24,13 @@ function Navbar() {
 
         <div className={`social-buttons ${isMenuOpen ? 'show' : ''}`}>
           <a href="https://www.instagram.com/gorkem_tand" target="_blank" rel="noopener noreferrer">
-            <img src="https://storage.googleapis.com/gorkemtandogan-images/Projects/Architect%20Portfolio/instagram.png" alt=''></img>
+            <img src="/images/instagram.png" alt=''></img>
           </a>
           <a href="https://twitter.com/GrkemTandgn" target="_blank" rel="noopener noreferrer">
-            <img src="https://storage.googleapis.com/gorkemtandogan-images/Projects/Architect%20Portfolio/twitter.png" alt=''></img>
+            <img src="/images/twitter.png" alt=''></img>
           </a>
           <a href="https://www.linkedin.com/in/gorkem-tandogan-5408a5183/" target="_blank" rel="noopener noreferrer">
-            <img src="https://storage.googleapis.com/gorkemtandogan-images/Projects/Architect%20Portfolio/linkedin.png" alt=''></img>
+            <img src="/images/linkedin.png" alt=''></img>
           </a>
         </div>
       </div>
